e9cd5b0cd1067a71570902b6d92e6585699eb021
JacobHowton <Jacob@howton.com>
2020-07-02 20:02:47 -0500

    Added Pygame and made a main window

--- PySnake.py
+++ PySnake.py
@@ -1,1 +1,20 @@
-print("Testing...")
+import pygame
+
+# initialize pygame
+pygame.init()
+
+# create the screen with height and width
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("Snake")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    pygame.display.update()
